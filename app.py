33bdcb3fa1484ee828b27ae548668dbcee5f8b77
chihnitha-vojjala <chikkivojjala@gmail.com>
2025-10-28 11:51:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('myForm.html')
+    return render_template('index.html')
 
 @app.route('/submit', methods=['POST'])
 def submit():
